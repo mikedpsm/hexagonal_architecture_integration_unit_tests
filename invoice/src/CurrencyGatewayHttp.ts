f34b41d221ffdb0708d5229bf68d0a510db97111
Maicon Douglas <maicon.medeiros@agilize.com.br>
2022-11-27 13:57:50 -0300

    Escopo basico do projeto de pratica

---
+++ invoice/src/CurrencyGatewayHttp.ts
@@ -0,0 +1,12 @@
+import CurrencyGateway from './CurrencyGateway';
+import axios from 'axios';
+import HttpClient from './HttpClient';
+
+export default class CurrencyGatewayHttp implements CurrencyGateway {
+  constructor(readonly httpClient: HttpClient, readonly baseUrl: string) {}
+
+  async getCurrencies(): Promise<any> {
+    const currencies = await this.httpClient.get(`${this.baseUrl}/currencies`);
+    return currencies;
+  }
+}
---
+++ invoice/src/main.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import CalculateInvoice from './CalculateInvoice';
+import TransactionDAODatabase from './TransactionDAODatabase';
+import CurrencyGatewayHttp from './CurrencyGatewayHttp';
+import AxiosAdapter from './AxiosAdapter';
+import PgPromiseAdapter from './PgPromiseAdapter';
+import InvoiceController from './InvoiceController';
+import ExpressAdapter from './ExpressAdapter';
+
+const connection = new PgPromiseAdapter();
+const transactionDAO = new TransactionDAODatabase(connection);
+const httpClient = new AxiosAdapter();
+const baseUrl = 'http://localhost:3001';
+const currencyGateway = new CurrencyGatewayHttp(httpClient, baseUrl);
+const calculateInvoice = new CalculateInvoice(transactionDAO, currencyGateway);
+const httpServer = new ExpressAdapter();
+new InvoiceController(httpServer, calculateInvoice);
+httpServer.listen(3000);
